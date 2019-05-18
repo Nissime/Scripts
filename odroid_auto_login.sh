6b07f4b77e7d9a8fc4c0e043c1c24873dd81dff8
nissime <nissim.eran@gmail.com>
2019-05-18 22:37:08 +0300

    Auto login with password to odroid

---
+++ odroid_auto_login.sh
@@ -0,0 +1,7 @@
+
+#using sshpass
+#sudo apt-get install sshpass
+#sshpass -p 'password' ssh user@server
+#sshpass -p 'password' ssh -o StrictHostKeyChecking=no  user@server
+
+sshpass -p 'odroid' ssh root@10.0.0.8
